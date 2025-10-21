46be4d79a6ded96c0551dfeb3519518c8940ad8d
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-10-22 03:22:50 +0530

    install sidebar

--- frontend/src/routes/sidebar-08/+page.svelte
+++ frontend/src/routes/sidebar-08/+page.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import AppSidebar from "$lib/components/app-sidebar.svelte";
 	import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
 	import { Separator } from "$lib/components/ui/separator/index.js";
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
 
 <Sidebar.Provider>
 	<AppSidebar />
@@ -33,11 +25,13 @@
 				</Breadcrumb.Root>
 			</div>
 		</header>
-		<div>
-			{@render children?.()}
-
+		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+			<div class="grid auto-rows-min gap-4 md:grid-cols-3">
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+			</div>
+			<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
 		</div>
 	</Sidebar.Inset>
 </Sidebar.Provider>
-
-
