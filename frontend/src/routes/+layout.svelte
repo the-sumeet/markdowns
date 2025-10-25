52889562fc3c53e6f3ac38b34fadace1e807edcf
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-10-26 00:09:21 +0530

    fmt

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,19 +6,18 @@
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import { onMount } from 'svelte';
-	import { GetCurrentFilesState } from "$lib/wailsjs/go/main/App";
+	import { GetCurrentFilesState } from '$lib/wailsjs/go/main/App';
 	import { appState } from '../store.svelte';
 	import type { main } from '$lib/wailsjs/go/models';
 
 	let { children } = $props();
 
 	onMount(() => {
-
 		GetCurrentFilesState().then((res: main.CurrentFilesState) => {
 			appState.currentDir = res.currentDir;
 			appState.currentFile = res.currentFile;
-		})
-	})
+		});
+	});
 </script>
 
 <svelte:head>
@@ -27,12 +26,13 @@
 
 <Sidebar.Provider class="h-screen">
 	<AppSidebar />
-	<Sidebar.Inset class="flex flex-col">
-		<header class="flex h-16 shrink-0 items-center gap-2">
+	<Sidebar.Inset class="flex flex-col max-w-full">
+		<header class="flex h-16 shrink-0 items-center gap-2 max-w-full">
 			<div class="flex items-center gap-2 px-4">
 				<Sidebar.Trigger class="-ml-1" />
 				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
-				<Breadcrumb.Root>
+				
+				<!-- <Breadcrumb.Root>
 					<Breadcrumb.List>
 						<Breadcrumb.Item class="hidden md:block">
 							<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
@@ -42,10 +42,10 @@
 							<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
 						</Breadcrumb.Item>
 					</Breadcrumb.List>
-				</Breadcrumb.Root>
+				</Breadcrumb.Root> -->
 			</div>
 		</header>
-		<div class="overflow-y-hidden max-h-full flex-1 p-2">
+		<div class="max-h-full flex-1 overflow-y-hidden p-2">
 			{@render children?.()}
 		</div>
 	</Sidebar.Inset>
