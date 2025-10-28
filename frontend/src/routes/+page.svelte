70002769918f6f147f78204354e66da85ea4ef7b
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-10-29 02:00:24 +0530

    add transition

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import * as InputGroup from '$lib/components/ui/input-group/index.js';
 	import MoreHorizontalIcon from '@lucide/svelte/icons/more-horizontal';
+	import Trash2 from '@lucide/svelte/icons/trash-2';
+	import { fade } from 'svelte/transition';
+
+	let hoveredCard = $state<number | null>(null);
 </script>
 
 <div class="relative flex h-full flex-col gap-8 p-2 md:px-10 xl:px-14">
@@ -13,7 +18,7 @@
 		</InputGroup.Addon>
 	</InputGroup.Root> -->
 
-	<div class="flex w-full flex-1 flex-col gap-12 overflow-y-auto pt-12 pb-32">
+	<div class="flex w-full flex-1 flex-col gap-12 overflow-y-auto pb-32 pt-12">
 		<div class="flex flex-col">
 			<h1 class="text-6xl font-extrabold">Foo</h1>
 			<p class="text-muted-foreground">/Foo/bar/baz/...</p>
@@ -21,8 +26,12 @@
 
 		<div class="flex w-full justify-center">
 			<div class="max-w-8xl flex flex-1 flex-wrap">
-				{#each Array(100) as i}
-					<div class="xl:1/4 w-full p-2 md:w-1/2 lg:w-1/3 xl:w-1/4">
+				{#each Array(100) as _, i}
+					<div
+						class="xl:1/4 w-full p-2 md:w-1/2 lg:w-1/3 xl:w-1/4"
+						onmouseenter={() => (hoveredCard = i)}
+						onmouseleave={() => (hoveredCard = null)}
+					>
 						<Card.Root class="">
 							<Card.Header>
 								<Card.Title>Card Title</Card.Title>
@@ -31,16 +40,29 @@
 							<Card.Content>
 								<p>Card Content</p>
 							</Card.Content>
-							<Card.Footer>
-								<p>Card Footer</p>
+							<Card.Footer class="flex justify-between">
+
+								{#if hoveredCard === i}
+									<div transition:fade={{ duration: 200 }}>
+										<Button size="icon" class="size-8 rounded-full">
+											<Trash2 />
+										</Button>
+									</div>
+								{:else}
+									<div class="invisible">
+										<Button size="icon" class="size-8 rounded-full">
+											<Trash2 />
+										</Button>
+									</div>
+								{/if}
 							</Card.Footer>
 						</Card.Root>
 					</div>
 				{/each}
 			</div>
-		</div>	
+		</div>
 
-		<div class="absolute inset-x-2 bottom-10 md:px-8 ">
+		<div class="absolute inset-x-2 bottom-10 md:px-8">
 			<InputGroup.Root class="!h-auto rounded-xl backdrop-blur  ">
 				<InputGroup.Input class="p-8 !text-xl" placeholder="Search in folder..." />
 				<InputGroup.Addon align="inline-end">
